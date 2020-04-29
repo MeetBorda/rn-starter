6f968c1883d15e510593a093459d1b8d8b54ab31
Meet Borda <borda.meet1@gmail.com>
2020-04-29 17:16:37 +0530

    Initial Commit

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+
+const ColorCounter = (props) => {
+   return (
+      <View>
+         <Text>{props.color}</Text>
+         <Button onPress={()=> props.onIncrease() } title={`Increase ${props.color}`}/>
+
+         <Button onPress={()=> props.onDecrease() } title={`Decrease ${props.color}`}/>
+      </View>
+   )
+};
+ 
+const styles = StyleSheet.create();
+
+export default ColorCounter;
+
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, StyleSheet, View , Button} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import ImageDetail from "../components/ImageDetail";
+
+const ImageScreen = () => {
+  return (
+    <View>
+         <ImageDetail
+            title="Forest"
+            imageSource={require('../../assets/forest.jpg')}
+            score={9}
+         />
+         <ImageDetail
+            title="Beach"
+            imageSource={require('../../assets/beach.jpg')}
+            score={7}
+         />
+         <ImageDetail
+            title="Mountain"
+            imageSource={require('../../assets/mountain.jpg')}
+            score={10}
+         />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create();
+
+export default ImageScreen;
+
